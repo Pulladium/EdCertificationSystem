bc00688cba827b831fad595459a0c60e9e657a33
Artem Vozhov <vozhov.artem1@gmail.com>
2024-11-13 21:35:29 +0100

    security.core @PreAuthorize for ctrl roles

--- data-service-mvc/src/main/java/com/vozh/art/dataservice/controller/CategoryController.java
+++ data-service-mvc/src/main/java/com/vozh/art/dataservice/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.vozh.art.dataservice.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -19,27 +20,31 @@ public class CategoryController {
     private final CategoryService categoryService;
 
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<CategoryResponse> createCategory(@RequestBody CategoryRequest request) {
         log.trace("CategoryController: Creating category: {}", request);
         return ResponseEntity.ok(categoryService.postNewCategory(request));
     }
-    //todo still should prevent infinite recursion
-    @GetMapping("/byId/{id}/depth/{depth}")
-    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id, @PathVariable int depth) {
-        log.trace("CategoryController: Getting category by id: {}", id);
-        return ResponseEntity.ok(categoryService.getCategoryResponseById(id, depth));
-    }
-    //todo still should prevent infinite recursion can cause ?stackoverflow
-    @GetMapping("/all/depth/{depth}")
-    public ResponseEntity<Set<CategoryResponse>> getAllCategories(@PathVariable int depth) {
-        log.trace("CategoryController: Getting all categories");
-        return ResponseEntity.ok(categoryService.getAllCategoriesRespWithSubCatDepth(depth));
-    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteCategoryById(@PathVariable Long id) {
         log.trace("CategoryController: Deleting category by id: {}", id);
         categoryService.deleteCategoryById(id);
         return ResponseEntity.ok("Category with id: " + id + " deleted");
+    }
+
+
+    @GetMapping("/byId/{id}/depth/{depth}")
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long id, @PathVariable int depth) {
+        log.trace("CategoryController: Getting category by id: {}", id);
+        return ResponseEntity.ok(categoryService.getCategoryResponseById(id, depth));
+    }
+
+    @GetMapping("/all/depth/{depth}")
+    public ResponseEntity<Set<CategoryResponse>> getAllCategories(@PathVariable int depth) {
+        log.trace("CategoryController: Getting all categories");
+        return ResponseEntity.ok(categoryService.getAllCategoriesRespWithSubCatDepth(depth));
     }
 }
