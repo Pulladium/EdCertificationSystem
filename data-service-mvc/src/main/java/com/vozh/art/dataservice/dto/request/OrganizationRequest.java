fa3197545ad27da32d50ab008d6484222814a62a
Artem Vozhov <vozhov.artem1@gmail.com>
2024-12-22 15:14:04 +0100

    org service base crud

--- data-service-mvc/src/main/java/com/vozh/art/dataservice/dto/request/OrganizationRequest.java
+++ data-service-mvc/src/main/java/com/vozh/art/dataservice/dto/request/OrganizationRequest.java
@@ -1,5 +1,6 @@
 package com.vozh.art.dataservice.dto.request;
 
+import com.vozh.art.dataservice.entity.Organization;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
@@ -17,7 +18,23 @@ public class OrganizationRequest {
     @NotBlank
     private String contactInfo;
 
+
 //    uuid assigned by system. status assigned by admin
 //    private Organization.OrganizationStatus status;
 //    private String maintainerKeycloakUUID;
+
+    public static OrganizationRequest fromOrganization(Organization organization){
+        return OrganizationRequest.builder()
+                .name(organization.getName())
+                .address(organization.getAddress())
+                .contactInfo(organization.getContactInfo())
+                .build();
+    }
+    public static Organization toOrganization(OrganizationRequest organizationRequest){
+        return Organization.builder()
+                .name(organizationRequest.getName())
+                .address(organizationRequest.getAddress())
+                .contactInfo(organizationRequest.getContactInfo())
+                .build();
+    }
 }
--- data-service-mvc/src/main/java/com/vozh/art/dataservice/service/OrganizationService.java
+++ data-service-mvc/src/main/java/com/vozh/art/dataservice/service/OrganizationService.java
@@ -3,6 +3,73 @@ package com.vozh.art.dataservice.service;
 import com.vozh.art.dataservice.dto.response.OrganizationResponse;
 import com.vozh.art.dataservice.entity.Organization;
 
+import com.vozh.art.dataservice.dto.request.OrganizationRequest;
+import com.vozh.art.dataservice.dto.response.OrganizationResponse;
+import com.vozh.art.dataservice.entity.Organization;
+import com.vozh.art.dataservice.repository.OrganizationRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class OrganizationService {
+
+    private final OrganizationRepository organizationRepository;
+
+    @Transactional
+    public OrganizationResponse createOrganization(OrganizationRequest request){
+        log.trace("Mapping request to organization: {}", request);
+        Organization organization = OrganizationRequest.toOrganization(request);
+        log.trace("Saving organization via organization_repository: {}", organization);
+        Organization savedOrganization = organizationRepository.save(organization);
+        log.trace("Mapping saved organization to response: {}", savedOrganization);
+        return OrganizationResponse.fromOrganization(savedOrganization);
+    }
+
+    public OrganizationResponse getOrganizationResponseById(Long id) {
+        log.trace("Getting organization by id: {}", id);
+        Organization organization = getOrganizationById(id);
+        return OrganizationResponse.fromOrganization(organization);
+    }
+
+    public boolean deleteOrganizationById(Long id) {
+        log.trace("Deleting organization by id: {}", id);
+        organizationRepository.deleteById(id);
+        return true;
+    }
+
+    @Transactional
+    public OrganizationResponse updateOrganization(OrganizationRequest request){
+        log.trace("Mapping request to organization: {}", request);
+        Organization organization = OrganizationRequest.toOrganization(request);
+        log.trace("Saving organization via organization_repository: {}", organization);
+        Organization savedOrganization = organizationRepository.save(organization);
+        log.trace("Mapping saved organization to response: {}", savedOrganization);
+        return OrganizationResponse.fromOrganization(savedOrganization);
+    }
+
+    @Transactional
+    public OrganizationResponse approveOrganization(Long id){
+        log.trace("Approving organization by id: {}", id);
+        Organization organization = getOrganizationById(id);
+        organization.setStatus(Organization.OrganizationStatus.APPROVED);
+        Organization savedOrganization = organizationRepository.save(organization);
+        return OrganizationResponse.fromOrganization(savedOrganization);
+    }
+
+
+    private Organization getOrganizationById(Long id) {
+        Organization organization = organizationRepository.findById(id).orElse(null);
+        if(organization == null){
+            log.error("Organization with id: {} not found", id);
+            //todo controler advice and exception handling
+            throw new RuntimeException("Organization with id: " + id + " not found");
+        }
+        return organization;
+    }
+
 
 }
